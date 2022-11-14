72d36e53b0c174f4cb8451b15b51f6df1b911240
Logan Engstrom <engstrom@mit.edu>
2022-11-14 12:13:47 -0500

    Create .gitignore

---
+++ lunch.py
@@ -0,0 +1,16 @@
+ok = [
+    'cft',
+    'cava',
+    'chipotle',
+    'malamamahlas',
+    'life alive',
+    'shy bird',
+    'flour',
+    'saloniki',
+    'student center',
+    'al\'s'
+]
+
+from random import shuffle
+shuffle(ok)
+print('=> ur lunch today: ', ok[1])
